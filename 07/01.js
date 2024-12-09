92ae13ed30ff88a233c1dae5eb3d8e1c927a1671
Kathrin Holzmann <lelith@gmx.de>
2024-12-09 09:08:02 +0100

    day 7 part 1

---
+++ 07/01.js
@@ -0,0 +1,57 @@
+const utils = require("../utils");
+
+const probeOperations = (operands, result) => {
+  const operations = ["+", "*"];
+  
+  const evaluate = (index, currentResult) => {
+    // when we reach the end of the operands array we check if the current result is equal to the expected result
+
+    if (index === operands.length) {
+      return currentResult === result;
+    }
+
+    // we iterate over the operations array and recursively call the evaluate function with the new result
+    for (const op of operations) {
+      let newResult;
+      if (op === "+") {
+        newResult = currentResult + operands[index];
+      } else if (op === "*") {
+        newResult = currentResult * operands[index];
+      }
+
+      if (evaluate(index + 1, newResult)) {
+        return true;
+      }
+    }
+
+    return false;
+  };
+
+  return evaluate(1, operands[0]);
+};
+
+const findEquations = (data) => {
+  const possibleEquationResults = [];
+  let sum = 0;
+  data.forEach((item) => {
+    const equation = item.split(":");
+    const result = parseInt(equation[0], 10);
+    const operands = equation[1].trim().split(" ").map((item) => parseInt(item, 10));
+    //console.log(result, operands);
+    if (probeOperations(operands, result)) {
+      possibleEquationResults.push(item);
+      sum+= result;
+    }    
+  });
+  //console.log(possibleEquationResults);
+  console.log('result', sum);
+}
+
+try {
+  // let data = utils.readInput('./example.txt');
+  let data = utils.readInput('./input.txt');
+  data = utils.modDataNewlineStr(data);
+  findEquations(data);
+} catch (e) {
+  console.log("Error", e.stack);
+}
